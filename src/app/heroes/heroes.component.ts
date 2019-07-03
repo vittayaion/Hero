c6d14ddfc4461069b843f44c01094893fa3219fe
vittayacom <vittaya.jame@gmail.com>
2019-07-03 09:48:49 +0700

    finish service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,18 +15,27 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
   
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() {
+  constructor(private heroService: HeroService) {
 
-  }
+
+   }
 
   ngOnInit() {
     // this.selectedHero = this.hero;
     // console.log(this.hero);
+    this.getHeroes();
   }
 
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 
   onSelect(p: Hero): void {
     this.selectedHero = p;
